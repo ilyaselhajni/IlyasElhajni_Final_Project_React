755e8533300b14b95d1064e7c09273026d67dba9
ilyaselhajni <ialhajni@gmail.com>
2025-06-04 13:42:32 +0100

    CONTACT-section

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
                 className="bg-cover bg-center h-48 sm:h-64 md:h-80 text-white flex items-center justify-center"
                 style={{ backgroundImage: `url(${images[1].image})` }}
             >
-                <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold text-center drop-shadow-lg'>ABOUT</h1>
+                <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold text-center drop-shadow-lg'>CONTACT</h1>
             </div>
         </div>
     );
